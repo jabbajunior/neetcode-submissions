bb67ab9d2e1aeb5ec2fcd155a154ee0b7f793a7a
Nishant Singh <60632647+jabbajunior@users.noreply.github.com>
2026-09-25 22:37:42 -0400

    Add: remove-element - submission-9

---
+++ Data Structures & Algorithms/remove-element/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # move all values matching val to end of nums array
+        # return k so that it matches all non val values
+
+        length = len(nums)
+
+        
+        start = 0
+        count = 0
+
+        for i in range(length):
+            # Every non val we move to front
+            if nums[i] != val:
+                nums[start] = nums[i]
+                start += 1
+                count += 1
+
+
+        return count
+
+        
+        # pUT BAD VALUES AT THE END
+        
+
